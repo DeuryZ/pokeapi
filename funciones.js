e3cb7570eebbc5e6574c703c4959e18aa8f25402
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-10-05 05:11:21 -0500

    feat: :sparkles: se agrego mostrar, buscar y gitignore

--- funciones.js
+++ funciones.js
@@ -1,11 +1,79 @@
 export async function cargarPokemones(myContent){
     let datosDePokeapi=(await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")).json())
-    console.log(datosDePokeapi)
     let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+    let urlDelPokemon
     for (let i=0;i<datosDePokeapi.results.length;i++){
-        console.log(datosDePokeapi.results[i]);
-        let urlDelPokemon
+        urlDelPokemon = datosDePokeapi.results[i].url;
+        let datosDeCadaPokemon = (await (await fetch(urlDelPokemon)).json());
+        console.log(datosDeCadaPokemon);
+        myContent.insertAdjacentHTML("beforeend",`
+        <div class="pokemonTypeContainer" pokeName="${datosDeCadaPokemon.name}"></div>
+        <h2>${datosDeCadaPokemon.name}</h2>
+        <img src="${datosDeCadaPokemon.sprites.front_default ? datosDeCadaPokemon.sprites.front_default:defaultImg}" alt="${datosDeCadaPokemon.name}">
+                ${datosDeCadaPokemon.types.map(data => `<span>${data.type.name}</span><br>`
+                ).join("")}
+        </div>
+        `);
     }
+}
+export async function buscarPokemones(myContentSearch,type){
+    let datosDePokeapi=(await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")).json())
+    let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+    let urlDelPokemon
+    for (let i=0;i<datosDePokeapi.results.length;i++){
+        urlDelPokemon = datosDePokeapi.results[i].url;
+        let datosDeCadaPokemon = (await (await fetch(urlDelPokemon)).json());
+        console.log(datosDeCadaPokemon);
+        datosDeCadaPokemon.types.map(data => {
+            console.log(type);
+            if (data==type){
+                console.log(data);
+                myContentSearch.insertAdjacentHTML("beforeend",`
+                <div class="pokemonTypeContainer" pokeName="${data.name}"></div>
+                <h2>${data.name}</h2>
+                <img src="${data.sprites.front_default ? data.sprites.front_default:defaultImg}" alt="${datosDeCadaPokemon.name}">
+                        ${data.types.map(data => `<span>${data.type.name}</span><br>`
+                        ).join("")}
+                </div>
+                `);
 
-
+            }
+        })
+        // myContent.insertAdjacentHTML("beforeend",`
+        // <div class="pokemonTypeContainer" pokeName="${datosDeCadaPokemon.name}"></div>
+        // <h2>${datosDeCadaPokemon.name}</h2>
+        // <img src="${datosDeCadaPokemon.sprites.front_default ? datosDeCadaPokemon.sprites.front_default:defaultImg}" alt="${datosDeCadaPokemon.name}">
+        //         ${datosDeCadaPokemon.types.map(data => `<span>${data.type.name}</span><br>`
+        //         ).join("")}
+        // </div>
+        // `);
+    }
+}
+export async function showListaDeTipos(){
+    let allTypesPokemon = [
+        'grass', 
+        'poison',
+        'fire', 
+        'flying', 
+        'water', 
+        'bug', 
+        'normal', 
+        'electric', 
+        'ground', 
+        'fairy', 
+        'fighting', 
+        'psychic', 
+        'rock', 
+        'steel', 
+        'ice', 
+        'ghost', 
+        'dragon', 
+        'dark'
+    ];
+    let myTypes=document.querySelector(".types");
+    allTypesPokemon.map(data=>{
+        myTypes.insertAdjacentHTML("beforeend", `
+        <li class="type" idName="${data}">${data}</li>
+        `);
+    })
 }
--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import {cargarPokemones} from "./funciones.js"
+import {showListaDeTipos} from "./funciones.js"
+import {buscarPokemones} from "./funciones.js"
 
 let myContent = document.querySelector(".content");
 let myContentSearch = document.querySelector(".contentSearch");
-addEventListener("DOMContentLoaded",cargarPokemones(myContent))
+
+addEventListener("DOMContentLoaded",
+    showListaDeTipos(),
+    cargarPokemones(myContent)
+);
+
+document.addEventListener("click",async(e) => {
+    e.preventDefault();
+    if (e.target.matches(".type")) {
+        myContent.style.display="none";
+        console.log(e.target.getAttribute("idName"));
+        buscarPokemones(myContentSearch,e.target.getAttribute("idName"));
+    }
+    
+}
+);
