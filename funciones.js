235e5438bbb412006c11b24b1fc9164018e6ed6a
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-10-07 16:46:50 -0500

    feat: :sparkles: Se agregaron las funciones para editar
    Se agregaron las funciones guardar pokemon, editar pokemon encargadas de conectar los datos a mockeapi para luego ser mostrados actualizados

--- funciones.js
+++ funciones.js
@@ -37,10 +37,9 @@ export async function buscarPokemones(myContentSearch,type){
         });
     }
 }
-
+//------------------
 export async function ventanaEmergentePokemons(namePokemon){
     let res = (await (await fetch("https://pokeapi.co/api/v2/pokemon/"+namePokemon)).json());
-    console.log(res);
     let img = res.sprites.front_default;
     let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
 
@@ -50,7 +49,6 @@ export async function ventanaEmergentePokemons(namePokemon){
         const valueDisplay = document.getElementById(statName);
         valueDisplay.textContent = input.value;
     }
-
     Swal.fire({
         title: `${namePokemon.toUpperCase()}`,
         text: "Modal with a custom image.",
@@ -72,26 +70,131 @@ export async function ventanaEmergentePokemons(namePokemon){
         
         }
     });
-
     const inputRanges = document.querySelectorAll('.range');
     inputRanges.forEach(input => {
         input.addEventListener('input', updateValue);
     });
     
 }
-export async function crearObjeto(val, name, statName){
-    let res = (await (await fetch("https://pokeapi.co/api/v2/pokemon/"+name)).json());
-    let datosDefault = {
-        name: res.name,
+
+
+
+export async function NuevaVentanaEmergentePokemons(namePokemon){
+    let res = (await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi")).json());
+    let defaultImg = "https://i.pinimg.com/originals/27/ae/5f/27ae5f34f585523fc884c2d479731e16.gif";
+    res.map(data=>{
+        if(data.name == namePokemon){
+            let img = data.img;
+            function updateValue(e) {
+                const input = e.target;
+                const statName = input.getAttribute("pokeStatName");
+                const valueDisplay = document.getElementById(statName);
+                valueDisplay.textContent = input.value;
+            }
+            Swal.fire({
+                title: `${namePokemon.toUpperCase()}`,
+                text: "Modal with a custom image.",
+                imageUrl: `${img ? img : defaultImg}`,
+                html: `
+                ${`<input max="150" type="range" class="range" value="${data.hp}" pokeStatName="hp" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="hp" idBase="hp">${data.hp}</b> HP </label><br>`}
+                ${`<input max="150" type="range" class="range" value="${data.attack}" pokeStatName="attack" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="attack" idBase="attack">${data.attack}</b> ATTACK </label><br>`}
+                ${`<input max="150" type="range" class="range" value="${data.defense}" pokeStatName="defense" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="defense" idBase="defense">${data.defense}</b> DEFENSE </label><br>`}
+                ${`<input max="150" type="range" class="range" value="${data.specialAttack}" pokeStatName="specialAttack" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="specialAttack" idBase="specialAttack">${data.specialAttack}</b> SPECIAL ATTACK </label><br>`}
+                ${`<input max="150" type="range" class="range" value="${data.specialDefense}" pokeStatName="specialDefense" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="specialDefense" idBase="specialDefense">${data.specialDefense}</b> SPECIAL DEFENSE </label><br>`}
+                ${`<input max="150" type="range" class="range" value="${data.speed}" pokeStatName="speed" idName="${namePokemon}" idMokapi="${data.id}"><label><b id="speed" idBase="speed">${data.speed}</b> SPEED </label><br>`}
+                `,
+                imageWidth: "80%",
+                imageHeight: "80%",
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Save',
+                denyButtonText: `Don't save`,
+                }).then((result) => {
+                if (result.isConfirmed) {
+                
+                } else if (result.isDenied) {
+                
+                }
+            });
+            const inputRanges = document.querySelectorAll('.range');
+            inputRanges.forEach(input => {
+                input.addEventListener('input', updateValue);
+            });
+        }
+    })
+}
+
+export async function seleccionVentanaEmergente(namePokemon){
+    let resMockapi=await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi")).json();
+    let validacion = false;
+    resMockapi.map(data=>{
+        if(data.name == namePokemon){
+            validacion=true;
+        }
+    });
+    if(!validacion){
+        ventanaEmergentePokemons(namePokemon); 
+    }
+    else{
+        NuevaVentanaEmergentePokemons(namePokemon);
+    }
+    
+}
+
+
+
+//------------------
+export async function guardarPokemon(name){
+    let res=await(await fetch("https://pokeapi.co/api/v2/pokemon/"+name)).json();
+    let resMockapi=await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi")).json();
+    let validacion = false;
+    resMockapi.map(data=>{
+        if(data.name == name){
+            validacion=true;
+        }
+    });
+    if(!validacion){
+        createPokemon(name);
+    }
+}
+async function createPokemon(name){
+    let res = await(await fetch("https://pokeapi.co/api/v2/pokemon/"+name)).json();
+    let data = {
+        name: name,
         hp: res.stats[0].base_stat,
         attack: res.stats[1].base_stat,
         defense: res.stats[2].base_stat,
         specialAttack: res.stats[3].base_stat,
         specialDefense: res.stats[4].base_stat,
-        speed: res.stats[5].base_stat
+        speed: res.stats[5].base_stat,
+        img: res.sprites.front_default,
     }
-    return datosDefault;
+    const config ={
+        method:"POST",
+        headers:{"Content-type":"application/json"},
+        body:JSON.stringify(data)
+    }
+    let resPost = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi", config)).json();
 }
+
+export async function editarPokemon(idPokemon, nombreStat, valorStat){
+    let data={}
+    if(nombreStat=="hp"){data.hp=valorStat}
+    if(nombreStat=="attack"){data.attack=valorStat}
+    if(nombreStat=="defense"){data.defense=valorStat}
+    if(nombreStat=="specialAttack"){data.specialAttack=valorStat}
+    if(nombreStat=="specialDefense"){data.specialDefense=valorStat}
+    if(nombreStat=="speed"){data.speed=valorStat}
+    console.log(data);
+    const config ={
+        method:"PUT",
+        headers:{"Content-type":"application/json"},
+        body:JSON.stringify(data)
+    }
+    let resPost = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/mokeapi/"+idPokemon, config)).json();
+}
+
+
 export async function showListaDeTipos(){
     let allTypesPokemon = [
         'grass', 
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import {cargarPokemones} from "./funciones.js"
 import {showListaDeTipos} from "./funciones.js"
 import {buscarPokemones} from "./funciones.js"
-import {ventanaEmergentePokemons} from "./funciones.js"
-import {crearObjeto} from "./funciones.js"
+import {seleccionVentanaEmergente} from "./funciones.js"
+import {guardarPokemon} from "./funciones.js"
+import { editarPokemon } from "./funciones.js"
 
 let myContent = document.querySelector(".content");
 let myContentSearch = document.querySelector(".contentSearch");
@@ -20,47 +21,15 @@ document.addEventListener("click",async(e) => {
     }
     if (e.target.matches(".pokemonTypeContainer")){
         let name = e.target.getAttribute("pokeName")
-        ventanaEmergentePokemons(name)
+        seleccionVentanaEmergente(name)
+        guardarPokemon(name);
     }
-    let datosNew={}
     if (e.target.matches(".range")){
-        let val = e.target.value;
-        let name = e.target.getAttribute("idName")
-        let statName = e.target.getAttribute("pokeStatName")
-        let datosDefault =await( crearObjeto(val, name, statName));
-        if (datosNew!={}){
-            if(statName == "hp"){
-                datosNew.hp = val;
-            }if(statName == "attack"){
-                datosNew.attack = val;
-            }if(statName == "defense"){
-                datosNew.defense = val;
-            }if(statName == "specialAttack"){
-                datosNew.specialAttack = val;
-            }if(statName == "specialDefense"){
-                datosNew.specialDefense = val;
-            }if(statName == "speed"){
-                datosNew.speed = val;
-            }  
-        }else{
-            if(statName == "hp"){
-                datosDefault.hp = val;
-            }if(statName == "attack"){
-                datosDefault.attack = val;
-            }if(statName == "defense"){
-                datosDefault.defense = val;
-            }if(statName == "specialAttack"){
-                datosDefault.specialAttack = val;
-            }if(statName == "specialDefense"){
-                datosDefault.specialDefense = val;
-            }if(statName == "speed"){
-                datosDefault.speed = val;
-            }    
-        }
-
-        console.log('====================================');
-        console.log(datosDefault);
-        console.log(datosNew);
-        console.log('====================================');
+        let nombreStat = e.target.getAttribute("pokestatname");
+        let valorStat = e.target.value;
+        let idPokemon = e.target.getAttribute("idMokapi");
+        editarPokemon(idPokemon, nombreStat, valorStat);
     }
 });
+
+
